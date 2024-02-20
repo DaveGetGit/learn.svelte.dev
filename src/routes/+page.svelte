4a1705563fcc2cbdf7370c3363800f2c96cb1115
Dave Get <daveget2020@gmail.com>
2024-02-20 17:32:33 +0100

    IF BLOCKS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,19 @@
     let name = 'Svelte';
     let src = "./0266554465.jpeg";
     let string = 'This is a sample <steong>string</strong>';
+        let count = 0;
 
-    let count = 0;
+    // $:if(count > 10){
+    //     alert('count is high');
+    //     count=0;
+    // }
+
+    $: doubled = count *2;
 
     function Increment(){
         count += 1;
     }
-
+ 
 </script>
 
 <h1>Hello {name.toUpperCase()}!</h1>
@@ -29,7 +35,18 @@
 <!-- HTML TAGS   NO SANITIZATION-->
 <p>{@html string}</p>
 
-<button on:click={Increment}>
-Click {count} times
+<button on:click={Increment} class='primary'>
+Click {count} {count === 1 ? 'time' : 'times'}   
+{doubled}
 </button>
+
+
+<!-- If BLOCKs -->
+{#if count > 10}
+{count} is greater than 10.
+{/if}
+
+
+
+
 
